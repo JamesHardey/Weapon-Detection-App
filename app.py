1a8b6331539c7b86fcca582999900189db4157a3
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-10-03 16:31:35 -0700

    update

--- app.py
+++ app.py
@@ -39,43 +39,43 @@ def main():
             st.image(detected_image, channels="BGR", caption="Object Detection Result")
 
     elif detection_mode == "Video":
-        uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov"])
+        # uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "avi", "mov"])
 
-        if uploaded_file is not None:
-            temp_file = tempfile.NamedTemporaryFile(delete=False)
-            temp_file.write(uploaded_file.read())
-            temp_file.close()
+        # if uploaded_file is not None:
+        #     temp_file = tempfile.NamedTemporaryFile(delete=False)
+        #     temp_file.write(uploaded_file.read())
+        #     temp_file.close()
 
-            # Open the temporary file using cv2.VideoCapture
-            video = cv2.VideoCapture(temp_file.name)
+        #     # Open the temporary file using cv2.VideoCapture
+        #     video = cv2.VideoCapture(temp_file.name)
 
-            fps = int(video.get(cv2.CAP_PROP_FPS))
-            codec = cv2.VideoWriter_fourcc(*"XVID")
-            output_file = "output.avi"
-            out = cv2.VideoWriter(output_file, codec, fps, (int(video.get(3)), int(video.get(4))))
+        #     fps = int(video.get(cv2.CAP_PROP_FPS))
+        #     codec = cv2.VideoWriter_fourcc(*"XVID")
+        #     output_file = "output.avi"
+        #     out = cv2.VideoWriter(output_file, codec, fps, (int(video.get(3)), int(video.get(4))))
 
-            # Initialize progress bar
-            progress_bar = st.progress(0)
+        #     # Initialize progress bar
+        #     progress_bar = st.progress(0)
 
-            frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-            for frame_index in range(frame_count):
-                ret, frame = video.read()
-                if not ret:
-                    break
+        #     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        #     for frame_index in range(frame_count):
+        #         ret, frame = video.read()
+        #         if not ret:
+        #             break
 
-                frame = detect_objects(frame)
-                out.write(frame)
+        #         frame = detect_objects(frame)
+        #         out.write(frame)
 
-                # Update progress bar
-                progress = (frame_index + 1) / frame_count
-                progress_bar.progress(progress)
+        #         # Update progress bar
+        #         progress = (frame_index + 1) / frame_count
+        #         progress_bar.progress(progress)
 
-            video.release()
-            out.release()
-            st.success("Object detection complete!")
+        #     video.release()
+        #     out.release()
+        #     st.success("Object detection complete!")
 
-            # Display the processed output video
-            st.video(output_file)
+        #     # Display the processed output video
+        #     st.video(output_file)
 
 
 if __name__ == "__main__":
